5f236fabc574d28c06f13ac99e17faa7408593d2
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-09-15 02:30:27 +0000

    Simple

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const prompt = require("prompt-sync")({ sigint: true });
+const collaborate = require("./collaborate");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const passcode = prompt("Enter your passcode for this session: ")
-app.get(`/${passcode}`, (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
-  console.log(`Access server at localhost:${PORT}/${passcode}`)
+  console.log(`Access server at localhost:${PORT}/`)
 });
