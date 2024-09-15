199e4d31df99fb728d30b291436941822139691d
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-09-15 02:29:56 +0000

    made code simple

--- collab/src/App.js
+++ collab/src/App.js
@@ -3,19 +3,11 @@ import MonacoEditor from "./MonacoEditor";
 import './App.css';
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/daniel")
-    .then((res) => res.json())
-    .then((data) => setTimeout(()=>setData(data.message)));
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <h2 className="App-title">Collaborate over Wi-Fi</h2>
-        <small>Edit this code box with others in real-time. Message: {data}</small>
+        <small>Edit this code box with others in real-time.</small>
       </header>
       <MonacoEditor />
     </div>
